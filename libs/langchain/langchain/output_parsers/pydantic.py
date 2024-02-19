707934d90eeaa47e34e97b6d83fca97d813a47ff
Xu Haohang <60390821+LAD021@users.noreply.github.com>
2024-02-19 20:48:00 +0800

    fix: Upgrade Pydantic for PydanticOutputParser

--- libs/langchain/langchain/output_parsers/pydantic.py
+++ libs/langchain/langchain/output_parsers/pydantic.py
@@ -4,7 +4,7 @@ from typing import Any, List, Type
 from langchain_core.exceptions import OutputParserException
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.outputs import Generation
-from langchain_core.pydantic_v1 import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError
 
 from langchain.output_parsers.format_instructions import PYDANTIC_FORMAT_INSTRUCTIONS
 
